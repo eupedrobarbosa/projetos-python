92e753981c7658c8e06550ce7ee55ffe4f902b34
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-07 15:25:54 -0300

    Create index copy.py

---
+++ projetos-python/index copy.py
@@ -0,0 +1,19 @@
+mesangem01 = input ('Olá, tudo bme?')
+
+mensagem02 = input ('Que bom. Vamos lá?')
+
+print ('Certo!')
+
+dia = input ('Que dia você nasceu?')
+mes = input ('Mês?')
+ano = input ('Ano?')
+
+print ('Certo, só para confirmar, você nasceu dia' , dia + ' de' , mes + ' de', ano)
+
+print ('OK')
+
+mensagem_nome = input ('Qual seu nome?')
+mensagem_idade = input ('Qual sua idade?')
+mensagem_estado_civil = input ('Qual seu estado civil?')
+
+print ('Certo, vou encaminhar seus dados e já começaremos sua entrevista')
