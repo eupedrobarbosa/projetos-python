88b2023e29de0042a35433f5c2d13505a4f94681
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-07 12:10:25 -0300

    Create index.py

---
+++ projetos-python/index.py
@@ -0,0 +1,7 @@
+nome = input ('Qual seu nome?')
+idade = input ('Qual sua idade?')
+peso = input ('Quanto você pesa?')
+
+print ('Confirmado..')
+
+print ('Seu nome é', nome + ',', idade + ' anos e', peso + 'kg')
