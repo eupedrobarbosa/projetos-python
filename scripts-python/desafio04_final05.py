4cc3bdebc6bc826212dd72f68609fe3eabc5eb53
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-08 13:48:53 -0300

    Create desafio04_final05.py

---
+++ scripts-python/desafio04_final05.py
@@ -0,0 +1,11 @@
+print ('Olá, seja bem-vindo ao mercadinho Pnzão!')
+
+mensagem01 = input ('Tudo bem senhor?')
+print ('Vamos lá?!')
+
+nome = input ('Qual seu nome?')
+idade = input ('Qual sua idade?')
+peso = input ('Qual seu peso?')
+treino = input ('O que você treina?')
+
+print ('Então seu nome é', nome + ', ', idade + ', ', peso + ' e você treina', treino)
