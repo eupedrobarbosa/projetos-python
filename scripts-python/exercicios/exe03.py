58cff449f9ea694bfc928935135684d1f7ae6db7
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-11 08:48:35 -0300

    Update exe03.py

--- scripts-python/exercicios/exe03.py
+++ scripts-python/exercicios/exe03.py
@@ -1,5 +1,5 @@
-n1 = int (input('Digite um valor'))
-n2 = int (input('Digite outro valor'))
-s = n1*n2
+n1 = int (input ('Digite um número:'))
+n2 = int (input ('Digite outro número:'))
+s = n1 + n2
 
-print ('A soma de {} e {} deu {}'.format(n1, n2, s))
+print ('A soma dos valor {} e {} resultou em {}'.format(n1, n2, s))
