206f49bb1d8511540eb6e3c8a416300bbdda6a17
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-17 19:34:11 -0300

    Update exe17.py

--- scripts-python/exercicios/exe17.py
+++ scripts-python/exercicios/exe17.py
@@ -1,6 +1,6 @@
 partidas = int (input ('Partidas: '))
 vitorias = int (input ('Vitórias: '))
 
-winrate = vitorias / partidas * 100
+winrate = (vitorias / partidas) * 100
 
 print ('A taxa de winrate é {:.2f}%'.format(winrate))
