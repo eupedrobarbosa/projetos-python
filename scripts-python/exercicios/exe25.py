108a43099021104deb72cf6f9e4e7570ed2d2e12
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-20 21:18:30 -0300

    Create exe25.py

---
+++ scripts-python/exercicios/exe25.py
@@ -0,0 +1,10 @@
+import random
+
+#sorteio time
+
+poteA = ['Barcelona', 'Real Madrid']
+poteB = ['Liverpool', 'City']
+
+gols = [0, 1, 2, 3, 4, 5]
+
+print (random.choice(poteA), random.choice(gols), '-', random.choice(gols), random.choice(poteB))
