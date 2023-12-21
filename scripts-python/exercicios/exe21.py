cc5525345cb1995e283d1578217e6c291f5beb60
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-21 16:37:53 -0300

    Update exe21.py

--- scripts-python/exercicios/exe21.py
+++ scripts-python/exercicios/exe21.py
@@ -1,7 +1,7 @@
-import math 
-
+from math import trunc
 #parte inteira de um número
+#podemos usar o from math import 
 
 num = float (input('Digite um número: '))
 
-print ('A parte inteira do número {} é {}'.format(num, math.trunc(num)))
+print ('A parte inteira do número {} é {}'.format(num, trunc(num)))
