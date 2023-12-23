90f8322aa1e6f3304a36c4f33eb870185a9bd9c8
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2023-12-23 19:46:15 -0300

    Update exe27.py

--- scripts-python/exercicios/exe27.py
+++ scripts-python/exercicios/exe27.py
@@ -1,8 +1,7 @@
 from math import hypot
-#calculando hipotenusa com importação
 
-co = float (input ('Digite o cateto oposto: '))
-ca = float (input ('Digite o cateto adjente: '))
-hi = hypot(co, ca)
+co = float (input ('Digite um cateto oposto: '))
+ca = float (input ('Digite um cateto adjente: '))
+hi = hypot (co, ca)
 
 print ('A hipotenusa vai ser {:.2f}'.format(hi))
