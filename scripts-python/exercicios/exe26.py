c282b5b4ccc47f4eac746507df3df3450532a81a
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2024-01-04 14:13:50 -0300

    Update exe26.py

--- scripts-python/exercicios/exe26.py
+++ scripts-python/exercicios/exe26.py
@@ -1,7 +1,7 @@
 from math import hypot 
 
 co = float (input ('Digite o cateto oposto: '))
-ca = float(input ('Digite o cateto adjente: '))
+ca = float(input ('Digite o cateto adjcente: '))
 hi = hypot(co, ca)
 
 print ('A hipotenusa vai ser {:.2f}'.format(hi))
