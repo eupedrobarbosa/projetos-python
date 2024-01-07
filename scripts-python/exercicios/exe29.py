faf423b21ee91c9ea9c9545b2af0ede5b96ee77d
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2024-01-07 13:31:43 -0300

    Update exe29.py

--- scripts-python/exercicios/exe29.py
+++ scripts-python/exercicios/exe29.py
@@ -10,9 +10,8 @@ area = com * lar #área total
 #informações do piso
 
 metro = int (input ('Digite o metro da caixa: '))
-
 caixas =  area / metro
 
 print (f'Área total: {area :.2f}m²')
 print (f'Piso: {metro}m²')
-print (f'Será necessário comprar {ceil(caixas)} caixas para cobrir uma área de {area :.2f}²')
+print (f'Será necessário comprar {ceil(caixas)} caixas de psio para cobrir uma área de {area :.2f}²')
