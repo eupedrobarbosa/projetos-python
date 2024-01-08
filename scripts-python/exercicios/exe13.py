0b0e020b3870ecd476a268528b29b81cc7916d3b
eu.pedrobarbosa <pedrobarbosahenrique03@gmail.com>
2024-01-08 20:03:54 -0300

    Update exe13.py

--- scripts-python/exercicios/exe13.py
+++ scripts-python/exercicios/exe13.py
@@ -1,10 +1,10 @@
 import pygame
 
-#tocando música com importações
+#Reproduzindo uma música com importações
 
 pygame.init()
 pygame.mixer.music.load('scripts-python\exercicios\kerosene.mp3')
-pygame.mixer.music.play()
+pygame.mixer.music.play
 input()
 pygame.event.wait()
 
